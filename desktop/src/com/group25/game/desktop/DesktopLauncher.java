61ee18fe8278d6894c4017757fca6457f39341ac
Sam Mear <sammear@pop-os.localdomain>
2021-12-07 22:32:52 +0000

    Initial Commit

---
+++ desktop/src/com/group25/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.group25.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.group25.game.PaperBagPrincess;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PaperBagPrincess(), config);
+	}
+}
