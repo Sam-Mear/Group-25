0473ea0013557f7757de2069eb5c160797fbde25
Sam Mear <sammear@pop-os.localdomain>
2021-12-08 03:10:21 +0000

    switched to lwjgpl3
    switched to lwjgpl3 for better compatability on later java versions.
    added basic screen rectangle to red background.

--- core/src/com/group25/game/PaperBagPrincess.java
+++ core/src/com/group25/game/PaperBagPrincess.java
@@ -1,25 +1,40 @@
 package com.group25.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import com.badlogic.gdx.math.Rectangle;
+
 public class PaperBagPrincess extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private OrthographicCamera camera;
+	private Rectangle character;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 500,500);
+
+		character = new Rectangle();//probably temp, just getting used to libgdx
+		character.x = 50;
+		character.y = 50;
+		character.width = 80;
+		character.height = 80;
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, character.x, character.y);
 		batch.end();
 	}
 	
--- desktop/src/com/group25/game/desktop/DesktopLauncher.java
+++ desktop/src/com/group25/game/desktop/DesktopLauncher.java
@@ -1,12 +1,23 @@
 package com.group25.game.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.group25.game.PaperBagPrincess;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PaperBagPrincess(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		// Limits FPS to the refresh rate of the currently active monitor.
+		config.useVsync(true);
+		config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        // If you remove the above line and set Vsync to false, you may get unlimited FPS, which can be
+        // useful for testing performance, but can also be very stressful to some hardware.
+        // You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+		
+		//set the window title and size.
+		config.setTitle("Paper Bag Princess");
+		config.setWindowedMode(900,700);
+		//launch the game
+		new Lwjgl3Application(new PaperBagPrincess(), config);
 	}
 }
