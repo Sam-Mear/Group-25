f6b1855c967d5273a4b010f4a23386616419146c
Mr-Lionnn <70093077+Mr-Lionnn@users.noreply.github.com>
2022-02-23 10:38:34 +0000

    Update Creature.java

--- core/src/com/group25/game/Creature.java
+++ core/src/com/group25/game/Creature.java
@@ -1,11 +1,15 @@
 package com.group25.game;
 
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import java.awt.*;
+import java.util.Collection;
+import java.util.Collections;
 
 public abstract class Creature extends GameEntity{
 
+    private Rectangle DamageArea;
     protected float speed;
     protected int health;
     protected float attackSpeed;
@@ -21,6 +25,39 @@ public abstract class Creature extends GameEntity{
         super(positionX, positionY, width, height, img);
         this.health = health;
         this.speed = entitySpeed;
+    }
+
+    //Attack Section
+    public void DamageArea(int takeDamage){
+        if (this.getDamageArea().contains(Creature.getposition())) {
+            if (this.getY() < Creature.getY()) {
+
+                this.health = this.health - takeDamage;
+            }
+
+            if (this.getY() > Creature.getY()) {
+
+                this.health = this.health - takeDamage;
+            }
+
+            if (this.getX() < Creature.getY()) {
+
+                this.health = this.health - takeDamage;
+            }
+
+            if (this.getX() > Creature.getY() ) {
+
+                this.health = this.health - takeDamage;
+            }
+        }
+    }
+
+    private static Object getposition() {
+        return getposition();
+    }
+
+    private Collection getDamageArea() {
+        return Collections.singleton(health);
     }
 
     public void editHealth(int edit){
