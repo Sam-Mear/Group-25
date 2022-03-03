7dc9dde173890bbf5862f5b82589ebdbb0f3adad
FilipZajac <78360371+FilipZajac@users.noreply.github.com>
2022-03-03 17:29:02 +0000

    Moving movement to enemy abstract

--- core/src/com/group25/game/Slime.java
+++ core/src/com/group25/game/Slime.java
@@ -7,15 +7,15 @@ import java.util.Random;
 
 public class Slime extends Enemy{
 
-    private double xSpeed = 1;
-    private double ySpeed = 1;
-    private Random r;
+//    private double xSpeed = 1;
+//    private double ySpeed = 1;
+//    private Random r;
 
     public Slime(float positionX, float positionY,int width, int height,int health, Sprite img, float entitySpeed){
         super(positionX, positionY, width, height, health, img, entitySpeed,
                 new Rectangle((int)positionX,(int)positionY,width,height),  //Hitbox
                 new Rectangle((int)positionX,(int)positionY,200,200)); //AlertArea
-        r = new Random();
+//        r = new Random();
     }
 
     @Override
@@ -25,21 +25,14 @@ public class Slime extends Enemy{
        // System.out.printf("Slime alertArea left x: %d bottom y: %d\n", (int) (this.getX() - (200 - this.getWidth()) / 2), (int) (this.getY() - (200 - this.getHeight()) / 2));
        // System.out.printf("Slime alertArea right x: %d top y: %d\n", (int) (this.getX() - (200 - this.getWidth()) / 2) + 200, (int) (this.getY() - (200 - this.getHeight()) / 2) + 200);
        // System.out.printf("Slime alertArea: width: %d height: %d\n", 200, 200);
-       // this.chasePlayer(player);
-
-        double xTemp = xSpeed;
-        double yTemp = ySpeed;
-        double deltaA = changeAngle(100,r.nextDouble()) * 2*Math.PI;
-        ySpeed = xTemp*Math.sin(deltaA)+yTemp*Math.cos(deltaA);
-        xSpeed = xTemp*Math.cos(deltaA)-yTemp*Math.sin(deltaA);
-
-        this.x += xSpeed;
-        this.y += ySpeed;
+       this.chasePlayer(player);
+       this.updateMovement();
     }
 
-    public double changeAngle(double b,double x){
-        double top = 1;
-        double bottom = 1+Math.exp(-b*Math.tan(Math.PI*(x-0.5)));
-        return top/bottom;
-    }
+//    public double changeAngle(double b,double x){
+//        double top = 1;
+//        double bottom = 1+Math.exp(-b*Math.tan(Math.PI*(x-0.5)));
+//        return top/bottom;
+//    }
+
 }
